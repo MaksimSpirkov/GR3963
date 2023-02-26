2c4c857c5f789584e3663ea814787af0bc8f2dcf
MaksimSpirkov <spirkov35@gmail.com>
2023-02-26 22:27:46 +0300

    Семинар 4.1

---
+++ Sem4Task24/Program.cs
@@ -0,0 +1,18 @@
+﻿//Программа, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+Console.Write("Введите число: ");
+string? inputLine = Console.ReadLine();
+if(inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+    int startNumber = 2;
+    string outLine = string.Empty;
+
+    while (startNumber < inputNumber)
+    {
+        outLine = outLine + startNumber + ", ";
+        startNumber = startNumber + 1;
+    }
+
+    Console.Write(outLine);
+}
