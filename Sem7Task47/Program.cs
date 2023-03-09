6cfe09f9d733863418fc94a590ef490b039039df
MaksimSpirkov <spirkov35@gmail.com>
2023-03-09 15:49:25 +0300

    Семинар 7

---
+++ Sem7Task47/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+
+Console.Clear();
+
+// Задаем размеры массива
+Console.Write("Введите количество строк: ");
+int linesVol = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columnsVol = Convert.ToInt32(Console.ReadLine());
+
+// Создаем двумерный массив
+double[,] array = new double[linesVol, columnsVol];
+
+// Заполняем массив случайными вещественными числами
+Random random = new Random(); // создаем генератор случайных чисел
+for (int i = 0; i < linesVol; i++)
+{
+    for (int j = 0; j < columnsVol; j++)
+    {
+        array[i, j] = Convert.ToDouble(new Random().Next(-10, 10)); // заполняем ячейку случайным вещественным числом
+    }
+}
+
+// Выводим массив на экран
+for (int i = 0; i < linesVol; i++)
+{
+    for (int j = 0; j < columnsVol; j++)
+    {
+        Console.Write(array[i, j] + "\t"); // выводим значение ячейки на экран
+    }
+    Console.WriteLine(); // переходим на новую строку
+}
---
+++ Sem7Task50/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая на вход принимает позиции 
+// элемента в двумерном массиве, и возвращает значение этого 
+// элемента или же указание, что такого элемента нет.
+
+Console.Clear();
+
+int[,] arr = new int[10, 10]; // создаем двумерный массив
+
+Random rnd = new Random(); // создаем экземпляр класса Random
+
+// заполняем массив случайными числами от 0 до 10
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = rnd.Next(10); // генерируем случайное число от 0 до 9
+    }
+}
+
+// выводим массив на экран
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        Console.Write(arr[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Введите номер строки: ");
+int row = int.Parse(Console.ReadLine()); // считываем номер строки
+Console.Write("Введите номер столбца: ");
+int col = int.Parse(Console.ReadLine()); // считываем номер столбца
+
+if (row >= arr.GetLength(0) || col >= arr.GetLength(1)) // проверяем, что введенные номера не выходят за границы массива
+{
+    Console.WriteLine("Такого элемента нет");
+}
+else
+{
+    Console.WriteLine($"Значение элемента: {arr[row, col]}"); // выводим значение элемента
+}
